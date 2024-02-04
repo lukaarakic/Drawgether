84158722b2996546a954dd1277f5990a04c01706
Rakic0 <lukarakic0@protonmail.com>
2024-02-04 14:17:22 +0100

    added timer in /draw, no functionality yet

--- app/routes/_app+/play+/draw.tsx
+++ app/routes/_app+/play+/draw.tsx
@@ -8,6 +8,7 @@ import { themeStorage } from "~/utils/theme.server"
 import { useLoaderData } from "@remix-run/react"
 import Artboard from "~/components/artboard-module/Artboard"
 import BoxLabel from "~/components/ui/BoxLabel"
+import { useEffect, useState } from "react"
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
   return [
@@ -28,6 +29,26 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 const Draw = () => {
   const { theme } = useLoaderData<typeof loader>()
+  const [remainingSeconds, setRemainingSeconds] = useState(10)
+
+  const minutes = Math.floor(remainingSeconds / 60)
+  const seconds = remainingSeconds - minutes * 60
+
+  function formatTime(string: number, pad: string, length: number) {
+    return (new Array(length + 1).join(pad) + string).slice(-length)
+  }
+
+  const finalTime =
+    formatTime(minutes, "0", 2) + ":" + formatTime(seconds, "0", 2)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingSeconds((prev) => prev - 1)
+    }, 1000)
+    if (remainingSeconds <= 0) clearInterval(interval)
+
+    return () => clearInterval(interval)
+  }, [remainingSeconds])
 
   return (
     <div>
@@ -40,6 +61,23 @@ const Draw = () => {
         </p>
       </BoxLabel>
       <Artboard />
+
+      <div className="mt-8 flex flex-col items-center justify-center">
+        <BoxLabel className="w-[25.7rem]" degree={-1.3}>
+          <p
+            className="text-border text-border-lg px-12 text-65"
+            data-text={finalTime}
+          >
+            {finalTime}
+          </p>
+        </BoxLabel>
+        <p
+          className="text-border text-border-lg mt-2 text-25 text-blue"
+          data-text="Timer"
+        >
+          Timer
+        </p>
+      </div>
     </div>
   )
 }
